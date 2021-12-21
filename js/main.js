fe29be522ae551c028997fe28b2dcad78da97c72
Raplanta <alina.raplanta@yandex.ru>
2021-12-21 13:13:38 +0300

    3-5 labs

---
+++ js/main.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function () {
+  this.document.getElementById("load-time").innerHTML =
+    "Page load time is " +
+    (this.performance.now() / 1000).toFixed(3) +
+    " seconds";
+});
